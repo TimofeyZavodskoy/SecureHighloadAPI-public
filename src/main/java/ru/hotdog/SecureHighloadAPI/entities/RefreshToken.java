11eb58d906265dcf08098cce01d9f8de4a6f56c8
TimofeyZavodskoy <ambrasovskiytimofey@gmail.com>
2025-08-17 01:24:06 +1200

    created refresh token entity

--- src/main/java/ru/hotdog/SecureHighloadAPI/entities/RefreshToken.java
+++ src/main/java/ru/hotdog/SecureHighloadAPI/entities/RefreshToken.java
@@ -1,4 +1,28 @@
 package ru.hotdog.SecureHighloadAPI.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.Instant;
+
+@Entity
+@Data
+@Table(name = "refresh_tokens")
 public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String jti;
+
+    @Column(nullable = false)
+    private Instant expiresAt;
+
+    @Column(nullable = false)
+    private boolean revoked = false;
 }
