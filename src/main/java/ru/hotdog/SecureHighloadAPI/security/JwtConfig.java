60c53bc7af57ee73eae232752b6a4fe32e725fb7
TimofeyZavodskoy <ambrasovskiytimofey@gmail.com>
2025-08-17 01:29:26 +1200

    created access-refresh

--- src/main/java/ru/hotdog/SecureHighloadAPI/security/JwtConfig.java
+++ src/main/java/ru/hotdog/SecureHighloadAPI/security/JwtConfig.java
@@ -1,36 +1,78 @@
 package ru.hotdog.SecureHighloadAPI.security;
 
-import io.jsonwebtoken.JwtParser;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import ru.hotdog.SecureHighloadAPI.services.UserDetailsImpl;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.time.Instant;
+import java.util.Collections;
 import java.util.Date;
+import java.util.Map;
+import java.util.UUID;
 
 @Slf4j
 @Component
 public class JwtConfig {
+
     @Value("${api.app.secret}")
     private String secret;
-    @Value("${api.app.lifetime}")
-    private int lifetime;
 
-    public String generateToken(Authentication authentication) {
-        log.info("Generating token");
+    @Value("${api.app.accesslifetime}")
+    private int accessLifetime;
+
+    @Value("${api.app.refreshlifetime}")
+    private int refreshLifetime;
+
+    public String generateAccessToken(Authentication authentication) {
+        String jti = UUID.randomUUID().toString();
+        log.info("Generating accessToken");
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
         return Jwts.builder()
                 .setSubject(userDetails.getUsername())
+                .setId(jti)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + lifetime))
+                .setExpiration(new Date(System.currentTimeMillis() + accessLifetime))
+                .addClaims(Map.of("type", "access"))
                 .signWith(getSigninKey(), SignatureAlgorithm.HS512)
                 .compact();
+    }
+
+    // Перегрузка
+    public String generateAccessToken(String username) {
+        Authentication auth = new UsernamePasswordAuthenticationToken(username, null, Collections.emptyList());
+        return generateAccessToken(auth);
+    }
+
+    public JwtPair generateRefreshToken(Authentication authentication) {
+        log.info("Generating refreshToken");
+        String jti = UUID.randomUUID().toString();
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        String token = Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setId(jti)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + refreshLifetime))
+                .addClaims(Map.of("type", "refresh"))
+                .signWith(getSigninKey(), SignatureAlgorithm.HS512)
+                .compact();
+        return new JwtPair(token, jti);
+    }
+
+    // Перегрузка
+    public JwtPair generateRefreshToken(String username) {
+        Authentication auth = new UsernamePasswordAuthenticationToken(username, null, Collections.emptyList());
+        return generateRefreshToken(auth);
+    }
+
+    public Jws<Claims> parseToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(getSigninKey()).build().parseClaimsJws(token);
     }
 
     public String getUsernameFromToken(String token) {
@@ -41,7 +83,21 @@ public class JwtConfig {
         return parser.parseClaimsJws(token).getBody().getSubject();
     }
 
+    public boolean isRefresh(Jws<Claims> jws) {
+        return "refresh".equals(jws.getBody().get("type", String.class));
+    }
+
+    public Instant getExpiration(Jws<Claims> jws) {
+        return jws.getBody().getExpiration().toInstant();
+    }
+
+    public String getJti(Jws<Claims> jws) {
+        return jws.getBody().getId();
+    }
+
     private Key getSigninKey() {
         return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
     }
+
+    public record JwtPair(String token, String jti) {}
 }
