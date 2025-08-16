b0287038b46665d5d18d58b39953a1aa478b18b7
TimofeyZavodskoy <ambrasovskiytimofey@gmail.com>
2025-08-17 01:28:08 +1200

    created relaition many-to-many with users and roles(not realized)

--- src/main/java/ru/hotdog/SecureHighloadAPI/repositories/RoleRep.java
+++ src/main/java/ru/hotdog/SecureHighloadAPI/repositories/RoleRep.java
@@ -1,8 +1,10 @@
 package ru.hotdog.SecureHighloadAPI.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.hotdog.SecureHighloadAPI.entities.Role;
 
+@Repository
 public interface RoleRep extends JpaRepository<Role, Long> {
     Role findByName(String name);
 }
--- src/main/java/ru/hotdog/SecureHighloadAPI/repositories/UserRep.java
+++ src/main/java/ru/hotdog/SecureHighloadAPI/repositories/UserRep.java
@@ -1,12 +1,15 @@
 package ru.hotdog.SecureHighloadAPI.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.hotdog.SecureHighloadAPI.entities.User;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRep extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+    void deleteUserById(Long id);
     boolean existsUserByEmail(String email);
     boolean existsUserByUsername(String username);
 }
